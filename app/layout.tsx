487ee9b835ad00a6f27695a23692639f6e5f7842
santicano <canosantiago404@gmail.com>
2024-02-12 18:57:31 -0300

    añadido rentModal a layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -32,6 +33,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <RegisterModal />
           <LoginModal />
           <Navbar currentUser={currentUser} />
