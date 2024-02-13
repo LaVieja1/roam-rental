978461661bd65e5e733bdc30740db13eb81c7e7d
santicano <canosantiago404@gmail.com>
2024-02-13 15:43:48 -0300

    agregada función de marcar como favorito una propiedad

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
+import useFavorite from "../hooks/useFavorite";
 import { SafeUser } from "../types";
 
 interface HeartButtonProps {
@@ -10,8 +11,10 @@ interface HeartButtonProps {
 }
 
 const HeartButton = ({ listingId, currentUser }: HeartButtonProps) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
 
   return (
     <div
