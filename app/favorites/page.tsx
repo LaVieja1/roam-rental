8609f7c5516dc94094cfa174dcabe4c981d53feb
santicano <canosantiago404@gmail.com>
2024-02-14 13:37:59 -0300

    añadida pagina mis favoritos

---
+++ app/favorites/page.tsx
@@ -0,0 +1,30 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoritesClient from "./FavoritesClient";
+
+const ListingPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No hay propiedades favoritas"
+          subtitle="Para añadir una propiedad favorita, vuelve a la página de propiedades"
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default ListingPage;
